8a3571a864a7b59a609a370752fecbbf23856149
aboud20-meet <aboud20@meet.mit.edu>
2018-08-01 18:34:30 +0300

    yay!

--- event-listeners/event-listeners.js
+++ event-listeners/event-listeners.js
@@ -1,13 +1,13 @@
 // YOUR TASK: Add more pictures!
-var pictures = ['./imgs/dog.jpg'];
+var pictures = ['./imgs/dog.jpg','./imgs/cat.jpg','./imgs/cat2.jpg','./imgs/cat3.jpg','./imgs/cat4.jpg'];
 var currentIndex = 0;
-
+var photo=document.getElementsByTagName("img");
 function showNextPicture() {
   currentIndex++; // increment current picture
   // if currentIndex is too large, start from the beginning again
   if (currentIndex >= pictures.length) {
     currentIndex = 0;
   }
+  photo[0].src=pictures[currentIndex]
 
-  // YOUR TASK: Finish this function!
 }
--- html-events/html-events.js
+++ html-events/html-events.js
@@ -1,1 +1,5 @@
-// Put all your JavaScript in this file!
+function color() {
+	var col=prompt("pick a color!!!");
+	var back=document.getElementsByTagName("body");
+	back[0].style.backgroundColor= col;
+}
